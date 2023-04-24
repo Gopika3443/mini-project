8b9a88ff13def17a0e8b228cc1d857bc421eb192
Gopikav <gopikaprakash2002@gmail.com>
2023-04-25 00:27:36 +0530

    new

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [confirm, setConfirm] = useState('');
     const [name, setName] = useState('');
     const [phone, setPhone] = useState('');
 
@@ -16,7 +17,7 @@ export const Register = (props) => {
             <h2>Register</h2>
         <form className="register-form" onSubmit={handleSubmit}>
             <label htmlFor="name">full name</label>
-            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full Name" />
+            <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="full name" />
             <label htmlFor="email">email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="phone">phone number</label>
@@ -24,7 +25,7 @@ export const Register = (props) => {
             <label htmlFor="password">create password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <label htmlFor="password">confirm password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <input value={confirm} onChange={(e) => setConfirm(e.target.value)} type="password" placeholder="********" id="password" name="password" />
             <button type="submit">Sign Up</button>
         </form>
         <button className="noAcct" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
