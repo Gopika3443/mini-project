55428a25a83a1bad5517e8c7499d371087d2a043
Gopikav <gopikaprakash2002@gmail.com>
2023-06-20 11:46:57 +0530

    all

--- Tracernet/client/src/components/LostPeopleForm.js
+++ Tracernet/client/src/components/LostPeopleForm.js
@@ -7,6 +7,7 @@ const LostPeopleForm = () => {
   const [gender, setGender] = useState("");
   const [location, setLocation] = useState("");
   const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null); // State for storing the uploaded image
 
   const handleSubmit = () => {
     // Handle form submission logic here
@@ -15,74 +16,90 @@ const LostPeopleForm = () => {
     console.log("Gender:", gender);
     console.log("Location:", location);
     console.log("Description:", description);
+    console.log("Image:", image);
     // Reset form fields
     setName("");
     setAge("");
     setGender("");
     setLocation("");
     setDescription("");
+    setImage(null);
+  };
+
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
   };
 
   return (
     <div className="lostpage-container">
       <div className="lost-contain">
-    <form className="lost-people-form">
-      <h2>Report Lost Person</h2>
-      <div className="form-field">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          id="name"
-          placeholder="Enter name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
+        <form className="lost-people-form">
+          <h2>Report Lost Person</h2>
+          <div className="form-field">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="age">Age</label>
+            <input
+              type="number"
+              id="age"
+              placeholder="Enter age"
+              value={age}
+              onChange={(e) => setAge(e.target.value)}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="gender">Gender</label>
+            <select
+              id="gender"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            >
+              <option value="">Select gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+          </div>
+          <div className="form-field">
+            <label htmlFor="location">Location</label>
+            <input
+              type="text"
+              id="location"
+              placeholder="Enter location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              placeholder="Enter description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div className="form-field">
+            <label htmlFor="image">Image</label>
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              onChange={handleImageUpload}
+            />
+          </div>
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
       </div>
-      <div className="form-field">
-        <label htmlFor="age">Age</label>
-        <input
-          type="number"
-          id="age"
-          placeholder="Enter age"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="gender">Gender</label>
-        <select
-          id="gender"
-          value={gender}
-          onChange={(e) => setGender(e.target.value)}
-        >
-          <option value="">Select gender</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
-      <div className="form-field">
-        <label htmlFor="location">Location</label>
-        <input
-          type="text"
-          id="location"
-          placeholder="Enter location"
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-      </div>
-      <div className="form-field">
-        <label htmlFor="description">Description</label>
-        <textarea
-          id="description"
-          placeholder="Enter description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
-      </div>
-      <button onClick={handleSubmit}>Submit</button>
-    </form>
-    </div>
     </div>
   );
 };
