ecb6c1156612e805f22635a30e5720986ec6e3b5
Gopikav <gopikaprakash2002@gmail.com>
2023-06-26 23:34:54 +0530

    hi

--- Tracernet/client/src/components/Footer.jsx
+++ Tracernet/client/src/components/Footer.jsx
@@ -11,8 +11,8 @@ const Footer = () => {
               <div className="single-cta">
                 <i className="fas fa-map-marker-alt"></i>
                 <div className="cta-text">
-                  <h4>Find us</h4>
-                  <span>1010 Avenue, sw 54321, chandigarh</span>
+                  <h4>People Behind</h4>
+                  <span>Gopika.V<br></br>Najmunniza.N<br></br>Sumayya Sulaiman</span>
                 </div>
               </div>
             </div>
@@ -20,8 +20,8 @@ const Footer = () => {
               <div className="single-cta">
                 <i className="fas fa-phone"></i>
                 <div className="cta-text">
-                  <h4>Call us</h4>
-                  <span>9876543210 0</span>
+                  <h4>Reach us</h4>
+                  <span>College Of Engineering,<br></br>Karunagappally</span>
                 </div>
               </div>
             </div>
@@ -30,7 +30,7 @@ const Footer = () => {
                 <i className="far fa-envelope-open"></i>
                 <div className="cta-text">
                   <h4>Mail us</h4>
-                  <span>mail@info.com</span>
+                  <span>tracenet3@gmail.com</span>
                 </div>
               </div>
             </div>
@@ -40,19 +40,9 @@ const Footer = () => {
           <div className="row">
             <div className="col-xl-4 col-lg-4 mb-50">
               <div className="footer-widget">
-                <div className="footer-logo">
-                  <a href="index.html">
-                    <img src="https://i.ibb.co/QDy827D/ak-logo.png" className="img-fluid" alt="logo" />
-                  </a>
-                </div>
-                <div className="footer-text">
-                  <p>
-                    Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec
-                    tetur adipisicing elit, Lorem ipsum dolor sit amet.
-                  </p>
-                </div>
+                
+                
                 <div className="footer-social-icon">
-                  <span>Follow us</span>
                   <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
                   <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
                   <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a>
@@ -65,26 +55,23 @@ const Footer = () => {
                   <h3>Useful Links</h3>
                 </div>
                 <ul>
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">about</a></li>
-                  <li><a href="#">services</a></li>
-                  <li><a href="#">portfolio</a></li>
-                  <li><a href="#">Contact</a></li>
+                  <li><a href="/home">Home</a></li>
                   <li><a href="#">About us</a></li>
-                  <li><a href="#">Our Services</a></li>
-                  <li><a href="#">Expert Team</a></li>
+                  <li><a href="#">Cases</a></li>
+                  <li><a href="#">Add Cases</a></li>
+                  <li><a href="#">Feedback</a></li>
                   <li><a href="#">Contact us</a></li>
-                  <li><a href="#">Latest News</a></li>
                 </ul>
               </div>
             </div>
             <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
               <div className="footer-widget">
                 <div className="footer-widget-heading">
-                  <h3>Subscribe</h3>
+                  <h3>Ask Anything?</h3>
                 </div>
                 <div className="footer-text mb-25">
-                  <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                  <p>Don’t miss to ask your doubts!
+                     <br></br>Kindly fill the form below.</p>
                 </div>
                 <div className="subscribe-form">
                   <form action="#">
@@ -103,8 +90,8 @@ const Footer = () => {
             <div className="col-xl-6 col-lg-6 text-center text-lg-left">
               <div className="copyright-text">
                 <p>
-                  Copyright &copy; 2018, All Right Reserved{' '}
-                  <a href="https://codepen.io/anupkumar92/">Anup</a>
+                  Copyright &copy; 2023, All Right Reserved{' '}
+                  <a href="https://codepen.io/anupkumar92/">Tracenet</a>
                 </p>
               </div>
             </div>
